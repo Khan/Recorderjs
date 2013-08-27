6822b3cbdd366ba7ea33f86b91737ace39fb8bf9
Pamela Fox <pamela@khanacademy.org>
2013-08-26 22:41:39 -0700

    Now change the content of the files

--- multirecorder.js
+++ multirecorder.js
@@ -1,87 +1,242 @@
-(function(window){
+/* 
+  This library is based on https://github.com/mattdiamond/Recorderjs
 
-  var WORKER_PATH = 'recorderWorker.js';
+  This library is used for recording audio from a microphone, using HTML5 APIs.
+  Its specifically designed for recording multiple audio streams and combining
+  them later, but it can be used for single recording as well.
 
-  var Recorder = function(source, cfg){
+  MultiRecorder is the exposed object, and it is responsible for
+  asking permission for the microphone (via getUserMedia) and setting
+  up an AudioContext object for use by all the recordings.
+
+  When you call MultiRecorder's startRecording, it will create a new Recorder
+  instance and start that. The Recorder will connect to the stream and use
+  a WebWorker to process the data that comes in.
+
+  When you call MultiRecorder's finishRecording, it will ask the current
+  Recorder instance to stop, and send back to final recording info/WAV,
+  which you can then save in your own data structures, so that you can
+  do whatever you want with it (put it in an audio tag, upload, etc).
+
+  If you want to combine multiple Recorder instances into a WAV,
+  use MultiRecorder's combineRecordings.
+*/
+(function(window) {
+
+  var WORKER_PATH = "multirecorder-worker.js";
+
+  var Recorder = function(source, cfg) {
+
     var config = cfg || {};
     var bufferLen = config.bufferLen || 4096;
     this.context = source.context;
+    this.pausePoints = [];
     this.node = this.context.createJavaScriptNode(bufferLen, 2, 2);
     var worker = new Worker(config.workerPath || WORKER_PATH);
     worker.postMessage({
-      command: 'init',
+      command: "init",
       config: {
         sampleRate: this.context.sampleRate
       }
     });
-    var recording = false,
-      currCallback;
+    var recording = false;
 
-    this.node.onaudioprocess = function(e){
-      if (!recording) return;
-      worker.postMessage({
-        command: 'record',
+    function makeCallback(callback) {
+      if (!callback) {
+        throw new Error("Callback not set");
+      }
+      var callbackId = "RecorderCallback" +
+        Math.round(Math.random() * 1000001);
+      window[callbackId] = function(json) {
+        callback(json);
+        delete window[callbackId];
+      };
+      return callbackId;
+    }
+
+    function sendMessage(message, callback) {
+      if (callback) {
+        message.callbackId = makeCallback(callback);
+      }
+      worker.postMessage(message);
+    }
+
+    this.node.onaudioprocess = function(e) {
+      if (!recording) {
+        return;
+      }
+      sendMessage({
+        command: "record",
         buffer: [
           e.inputBuffer.getChannelData(0),
           e.inputBuffer.getChannelData(1)
         ]
       });
-    }
+    };
 
-    this.configure = function(cfg){
-      for (var prop in cfg){
-        if (cfg.hasOwnProperty(prop)){
+    this.configure = function(cfg) {
+      for (var prop in cfg) {
+        if (cfg.hasOwnProperty(prop)) {
           config[prop] = cfg[prop];
         }
       }
-    }
+    };
 
-    this.record = function(){
+    this.record = function() {
       recording = true;
-    }
+    };
 
-    this.stop = function(){
+    this.stop = function() {
       recording = false;
-    }
+    };
 
-    this.clear = function(){
-      worker.postMessage({ command: 'clear' });
-    }
+    this.isRecording = function() {
+      return recording;
+    };
 
-    this.getBuffer = function(cb) {
-      currCallback = cb || config.callback;
-      worker.postMessage({ command: 'getBuffer' })
-    }
+    this.clear = function() {
+      sendMessage({command: "clear"});
+    };
 
-    this.exportWAV = function(cb, type){
-      currCallback = cb || config.callback;
-      type = type || config.type || 'audio/wav';
-      if (!currCallback) throw new Error('Callback not set');
-      worker.postMessage({
-        command: 'exportWAV',
-        type: type
-      });
-    }
+    this.finishRecording = function(cb) {
+      var self = this;
 
-    worker.onmessage = function(e){
-      var blob = e.data;
-      currCallback(blob);
-    }
+      var onFinish = function(exported) {
+        self.wav = exported.wav;
+        self.samples = exported.samples;
+        cb(self);
+      };
+
+      sendMessage({
+        command: "finishRecording"
+      }, onFinish);
+    };
+
+    this.combineRecordings = function(cb, recordings) {
+      var self = this;
+
+      var samples = [];
+      for (var i = 0; i < recordings.length; i++) {
+        samples.push(recordings[i].samples);
+      }
+
+      var onFinish = function(exported) {
+        self.wav = exported.wav;
+        self.samples = exported.samples;
+        cb(self);
+      };
+
+      sendMessage({
+        command: "combineRecordings",
+        samples: samples
+      }, onFinish);
+    };
+
+    this.createAudioPlayer = function() {
+      var url = URL.createObjectURL(this.wav);
+      var audioElem = document.createElement("audio");
+      audioElem.controls = true;
+      audioElem.src = url;
+      return audioElem;
+    };
+
+    this.createDownloadLink = function() {
+      var url = URL.createObjectURL(this.wav);
+      var anchorElem = document.createElement("a");
+      anchorElem.href = url;
+      anchorElem.download = new Date().toISOString() + ".wav";
+      anchorElem.innerHTML = anchorElem.download;
+      return anchorElem;
+    };
+
+    worker.onmessage = function(e) {
+      var callbackId = e.data.callbackId;
+      if (callbackId) {
+        window[callbackId](e.data.result);
+      }
+    };
 
     source.connect(this.node);
-    this.node.connect(this.context.destination);    //this should not be necessary
+    this.node.connect(this.context.destination); //this should not be necessary
   };
 
-  Recorder.forceDownload = function(blob, filename){
-    var url = (window.URL || window.webkitURL).createObjectURL(blob);
-    var link = window.document.createElement('a');
-    link.href = url;
-    link.download = filename || 'output.wav';
-    var click = document.createEvent("Event");
-    click.initEvent("click", true, true);
-    link.dispatchEvent(click);
+  // We create this object so that we only
+  // have to request getUserMedia once per app
+  function MultiRecorder(config) {
+    var audioContext;
+    var source;
+    var currentRecording;
+
+    window.AudioContext = window.AudioContext || window.webkitAudioContext;
+    navigator.getUserMedia = navigator.getUserMedia ||
+      navigator.webkitGetUserMedia;
+    window.URL = window.URL || window.webkitURL;
+
+    this.isRecording = function() {
+      return currentRecording && currentRecording.isRecording();
+    };
+
+    this.startRecording = function(delaySeconds) {
+      var deferred = jQuery.Deferred();
+      if (!window.AudioContext || !navigator.getUserMedia) {
+        deferred.reject("Audio recording does not work in this browser");
+      }
+
+      function startRecordingForReal() {
+        currentRecording = new Recorder(source,
+          {workerPath: config.workerPath});
+
+        if (delaySeconds) {
+          var delayTimer = window.setInterval(function() {
+            if (delaySeconds === 0) {
+              deferred.resolve(currentRecording);
+              currentRecording.record();
+              window.clearInterval(delayTimer);
+            } else {
+              deferred.notify(delaySeconds);
+              delaySeconds--;
+            }
+          }, 1000);
+        } else {
+          currentRecording.record();
+        }
+      }
+
+      if (!audioContext) {
+        audioContext = new AudioContext();
+        navigator.getUserMedia({audio: true}, function(stream) {
+          source = audioContext.createMediaStreamSource(stream);
+          source.connect(audioContext.destination);
+          startRecordingForReal();
+        }, function() {
+          deferred.reject("Error getting mic input");
+        });
+      } else {
+        startRecordingForReal();
+      }
+      return deferred;
+    };
+
+    this.stopRecording = function() {
+      var deferred = jQuery.Deferred();
+      currentRecording.stop();
+      currentRecording.finishRecording(function() {
+        deferred.resolve(currentRecording);
+        });
+      return deferred;
+    };
+
+    this.combineRecordings = function(recordings) {
+      var deferred = jQuery.Deferred();
+      // This recorder is only used for its encoding functionality
+      var combined = new Recorder(source);
+      combined.combineRecordings(function() {
+        deferred.resolve(combined);
+        }, recordings);
+      return deferred;
+    };
   }
 
-  window.Recorder = Recorder;
+  window.MultiRecorder = MultiRecorder;
 
 })(window);
